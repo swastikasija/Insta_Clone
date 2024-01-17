8945491bf5c6521c036a6c7d50ca0328f8482aba
swastik <swastikasija072@gmail.com>
2024-01-17 22:26:16 +0530

    userSchema Updated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
 
-module.exports = router;
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  posts: [{ type:mongoose.Schema.Types.ObjectId, ref: "post" }]
+})
