428d1f00dcc2e0a9579a44007ab358a7a5bc3c0f
swastik <swastikasija072@gmail.com>
2024-01-22 21:30:37 +0530

    users.js updated

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,6 @@ const userSchema = mongoose.Schema({
   posts: [{ type:mongoose.Schema.Types.ObjectId, ref: "post" }]
 });
 
-console.log("swastik here");
 userSchema.plugin(plm)
 
 module.exports = mongoose.model("user", userSchema)
